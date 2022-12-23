2ca4b4c43730674db3f0b6cc619eb39b7b7762f0
VovaKyrylenko <110337169+VovaKyrylenko@users.noreply.github.com>
2022-12-23 21:56:04 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const parent = document.querySelector('.gallery');
+galleryItems.forEach(item => {
+  const wrapper = document.createElement('div');
+  wrapper.className = 'gallery__item';
+  const link = document.createElement('a');
+  link.className = 'gallery__link';
+  link.href = item.original;
+  const img = document.createElement('img');
+  img.src = item.preview;
+  img.alt = item.description;
+  img.className = 'gallery__image';
+  img.setAttribute('data-source', item.original);
+  link.appendChild(img);
+  wrapper.appendChild(link);
+  parent.appendChild(wrapper);
+});
 
-console.log(galleryItems);
+parent.addEventListener('click', evt => {
+  evt.preventDefault();
+  if (evt.target.classList.contains('gallery__image')) {
+    let lightbox = new SimpleLightbox('.gallery a');
+  }
+});
